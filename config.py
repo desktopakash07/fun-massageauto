03b8972904c26c6c9aac204957e3363753c4b14c
desktopakash07 <desktopakash07@gmail.com>
2025-08-15 09:25:20 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Replace these with your actual credentials
+api_id = 25547287
+api_hash = 'b79197bb0c7badbcfe5cc12c9fb9dbc4'
+phone_number = '+918787680944'
+
+# Group username (with @) or group ID
+target_group = '@FUNToken_OfficialChat'
